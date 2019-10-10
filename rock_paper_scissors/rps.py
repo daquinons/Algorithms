c830b1028e54a9ead012b7aee97c8380ef2216f2
David Quiñones <daquinons@users.noreply.github.com>
2019-10-10 13:31:33 +0200

    Solve rock_paper_scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python
 
 import sys
+import itertools
+
 
 def rock_paper_scissors(n):
-  pass 
+    options = ["rock", "paper", "scissors"]
+    solution = [list(option)
+                for option in list(itertools.product(options, repeat=n))]
+    return solution
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
